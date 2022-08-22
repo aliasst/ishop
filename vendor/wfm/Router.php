04e27ed7e963e0dcb1185c9206381f72fdb7084e
aliasst <ahtubafishing@yandex.ru>
2022-08-22 20:24:57 +0300

    Lesson 7

--- vendor/wfm/Router.php
+++ vendor/wfm/Router.php
@@ -24,7 +24,54 @@ class Router
 
     public static function dispatch ($url)
     {
+        if(self::matchRoute($url)){
+            echo "OK";
+        } else {
+            echo 'NO';
+        }
+    }
 
+
+    public static function matchRoute ($url): bool
+    {
+        debug($url);
+
+        foreach(self::$routes as $pattern => $route) {
+            if(preg_match("#{$pattern}#", $url, $matches)){
+                foreach($matches as $k => $v){
+                    if(is_string($k)) {
+                        $route[$k] = $v;
+                    }
+                }
+                if(empty($route['action'])){
+                    $route['action'] = 'index';
+                }
+                if(!isset($route['admin_prefix'])){
+                    $route['admin_prefix'] = '';
+                } else {
+                    $route['admin_prefix'] = '\\';
+                }
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                $route['action'] = self::lowerCamelCase($route['action']);
+                debug($route);
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    protected static function upperCamelCase($name): string
+    {
+        $name = str_replace('-', ' ', $name);
+        $name = ucwords($name);
+        $name = str_replace(' ', '', $name);
+        return $name;
+    }
+
+    protected static function lowerCamelCase($name): string
+    {
+        return lcfirst(self::upperCamelCase($name));
     }
 
 

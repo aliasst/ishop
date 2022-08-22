47046cc6185e40604afeb61da142b6583fad332d
aliasst <ahtubafishing@yandex.ru>
2022-08-22 10:59:35 +0300

    Lesson 4

---
+++ config/params.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'admin_email' => 'ahtubafishing@yandex.ru',
+    'site_name' => 'E-shop',
+    'pagination' => 3
+];
--- public/index.php
+++ public/index.php
@@ -5,3 +5,7 @@ if(PHP_MAJOR_VERSION < 8) {
 
 
 require_once dirname(__DIR__) .'/config/init.php';
+
+new \wfm\App();
+
+var_dump(\wfm\App::$app->getProperties());

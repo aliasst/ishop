8e29e2c9c40853a3b18f949cd05f6c294efe0edb
aliasst <ahtubafishing@yandex.ru>
2022-08-22 13:50:54 +0300

    Lesson 6

--- public/index.php
+++ public/index.php
@@ -5,10 +5,14 @@ if(PHP_MAJOR_VERSION < 8) {
 
 
 require_once dirname(__DIR__) .'/config/init.php';
+require_once HELPERS .'/functions.php';
+require_once CONFIG .'/routes.php';
 
 new \wfm\App();
 
 //var_dump(\wfm\App::$app->getProperties());
+
+debug(\wfm\Router::getRoutes());
 
 
 
--- vendor/wfm/App.php
+++ vendor/wfm/App.php
@@ -2,18 +2,24 @@
 
 namespace wfm;
 
+use http\QueryString;
+
 class App
 {
     public static $app;
 
     public function __construct ()
     {
+        $query = trim(urldecode($_SERVER['QUERY_STRING']), '/');
+
         new ErrorHandler();
         self::$app = Registry::getInstance();
         $this->getParams();
+        Router::dispatch($query);
     }
 
-    protected function getParams() {
+    protected function getParams()
+    {
 
         $params = require_once CONFIG . '/params.php';
         if(!empty($params)) {
@@ -24,5 +30,7 @@ class App
 
 
     }
+
+
 
 }

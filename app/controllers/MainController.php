a94e835c59c53229990cffb57e539db5aaaacc67
aliasst <ahtubafishing@yandex.ru>
2022-09-09 10:38:59 +0300

    Lesson 2/3

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,20 +3,24 @@
 namespace app\controllers;
 
 use app\models\Main;
+use RedBeanPHP\R;
 use wfm\Controller;
 
 /** @property Main $model */
 
-class MainController extends Controller
+class MainController extends AppController
 {
     public function indexAction () {
 
-       $names = $this->model->get_names();
+        $slides = R::findAll('slider');
+
+        $names = $this->model->get_names();
 
 
 
         $this->setMeta("Главная", 'Описание для главной страницы', 'Главная, магазин');
-        $test = 'Тестовые даные';
-        $this->set(compact('test'));
+
+
+        $this->set(compact('slides'));
     }
 }

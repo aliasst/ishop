318ccebb2a1e3ddcdfd3600c9e99003940ad0a76
aliasst <ahtubafishing@yandex.ru>
2022-10-02 20:26:49 +0300

    lesson 2/19

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -2,11 +2,27 @@
 
 namespace app\controllers;
 
+use app\models\Product;
+use wfm\App;
+
+/** @property Product $model */
+
 class ProductController extends AppController
 {
     public function viewAction() {
 
+        $lang = App::$app->getProperty('language');
+
+        $product = $this->model->get_product($this->route['slug'], $lang);
+        //debug($product);
+        if(!$product){
+            throw new \Exception('Такого товара не существует', 404);
+        }
+
+        $gallery = $this->model->get_gallery($product['id']);
 
 
+        $this->setMeta($product['title'],$product['description'],$product['keywords']);
+        $this->set(compact('product', 'gallery'));
     }
 }

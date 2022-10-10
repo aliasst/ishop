26f4c27f5418dc49284f96d5ca31ed800cd883f9
aliasst <ahtubafishing@yandex.ru>
2022-10-10 17:49:34 +0300

    lesson 2/39

--- config/params.php
+++ config/params.php
@@ -2,5 +2,13 @@
 return [
     'admin_email' => 'ahtubafishing@yandex.ru',
     'site_name' => 'E-shop',
-    'pagination' => 3
+    'pagination' => 3,
+
+    'smtp_host' => 'smtp.yandex.ru',
+    'smtp_auth' => true,
+    'smtp_port' => 465,
+    'smtp_username' => 'logunova@thestreltsovs.ru',
+    'smtp_password' => 'privet!Nataliya',
+    'smtp_secure' => 'ssl', //ssl
+    'smtp_from_email' => 'logunova@thestreltsovs.ru',
 ];

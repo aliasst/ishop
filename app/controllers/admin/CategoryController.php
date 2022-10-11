15a856863ef280d901a537009c02bb91eea96848
aliasst <ahtubafishing@yandex.ru>
2022-10-11 18:18:36 +0300

    lesson 3/6

--- app/controllers/admin/CategoryController.php
+++ app/controllers/admin/CategoryController.php
@@ -2,14 +2,40 @@
 
 namespace app\controllers\admin;
 
+use RedBeanPHP\R;
+
 class CategoryController extends AppController
 {
 
-    public function indexAction() {
+    public function indexAction()
+    {
 
         $title = 'Список категорий';
         $this->setMeta('Список категорий');
         $this->set(compact('title'));
+    }
+
+    public function deleteAction()
+    {
+        $id = get('id');
+        $errors = '';
+        $children = R::count('category', 'parent_id = ?', [$id]);
+        $products = R::count('product', 'category_id = ?', [$id]);
+        if($children){
+            $errors  .= "Ошибка! В категории есть вложенные категории<br>";
+        }
+        if($products){
+            $errors  .= "Ошибка! В категории есть товары";
+        }
+        if($errors){
+            $_SESSION['errors'] = $errors;
+            redirect();
+        }
+        R::exec("DELETE FROM category WHERE id = ?", [$id]);
+        R::exec("DELETE FROM category_description WHERE category_id = ?", [$id]);
+
+        $_SESSION['success'] = 'Категория удалена';
+        redirect();
     }
 
 }

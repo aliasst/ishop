098f968246453482c9e8e618415a5f694283fb96
aliasst <ahtubafishing@yandex.ru>
2022-10-13 08:54:48 +0300

    lesson 3/18

---
+++ app/controllers/admin/DownloadController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\controllers\admin;
+
+
+use app\models\admin\Download;
+use RedBeanPHP\R;
+use wfm\App;
+use wfm\Pagination;
+
+/** @property Download $model */
+class DownloadController extends AppController
+{
+
+
+    public function indexAction()
+    {
+        $lang = App::$app->getProperty('language');
+        $page = get('page');
+        $perpage = App::$app->getProperty('pagination');
+
+        $total = R::count('download');
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+        $downloads = $this->model->get_downloads($lang, $start, $perpage);
+        $title = 'Список файлов';
+        $this->setMeta('Список файлов');
+        $this->set(compact('title','downloads', 'pagination', 'total'));
+    }
+}
--- app/views/admin/parts/sidebar.php
+++ app/views/admin/parts/sidebar.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="<?= PATH ?>" class="brand-link" target="_blank">
         <img src="<?= PATH ?>/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Перейти на сайт</span>
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
     <!-- Sidebar -->
@@ -14,8 +14,8 @@
                 <img src="<?= PATH ?>/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="<?= ADMIN ?>/user/edit?id=<?= $_SESSION['user']['id'] ?>" class="d-block"><?= $_SESSION['user']['name'] ?></a>
-                <a href="<?= ADMIN ?>/user/logout" class="d-block">Выход</a>
+                <a href="<?= ADMIN ?>/user/edit?id=<?= $_SESSION['user']['id'] ?>" class="d-block"><?= h($_SESSION['user']['name']); ?></a>
+                <a href="<?= ADMIN ?>/user/logout" class="d-block">Logout</a>
             </div>
         </div>
 
@@ -34,26 +34,70 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-                     with font-awesome or any other icon font library -->
+
                 <li class="nav-item">
                     <a href="<?= ADMIN ?>" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Главная</p>
                     </a>
                 </li>
+
                 <li class="nav-item">
                     <a href="<?= ADMIN ?>/category" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
                         <p>Категории</p>
                     </a>
                 </li>
+
                 <li class="nav-item">
                     <a href="<?= ADMIN ?>/product" class="nav-link">
-                        <i class="nav-icon fas fa-th"></i>
+                        <i class="nav-icon fas fa-barcode"></i>
                         <p>Товары</p>
                     </a>
                 </li>
+
+                <li class="nav-item">
+                    <a href="<?= ADMIN ?>/download" class="nav-link">
+                        <i class="nav-icon fas fa-file-upload"></i>
+                        <p>Файлы</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= ADMIN ?>/order" class="nav-link">
+                        <i class="nav-icon fas fa-shopping-bag"></i>
+                        <p>Заказы</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= ADMIN ?>/user" class="nav-link">
+                        <i class="nav-icon fas fa-user-friends"></i>
+                        <p>Пользователи</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= ADMIN ?>/cache" class="nav-link">
+                        <i class="nav-icon fas fa-database"></i>
+                        <p>Управление кэшем</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= ADMIN ?>/page" class="nav-link">
+                        <i class="nav-icon far fa-file-alt"></i>
+                        <p>Страницы</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= ADMIN ?>/slider" class="nav-link">
+                        <i class="nav-icon far fa-image"></i>
+                        <p>Слайдер</p>
+                    </a>
+                </li>
+
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
